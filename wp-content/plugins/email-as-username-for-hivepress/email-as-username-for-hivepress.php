778becebed0725f8fd5b554d28977994d979519c
Alex Kostritsa <kostritsaalex@gmail.com>
2024-11-21 19:00:15 +0200

    email s username plugin - update

--- wp-content/plugins/email-as-username-for-hivepress/email-as-username-for-hivepress.php
+++ wp-content/plugins/email-as-username-for-hivepress/email-as-username-for-hivepress.php
@@ -9,23 +9,44 @@
  * Domain Path: /languages/
  */
 
+
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-// Register extension directory.
-add_filter(
-	'hivepress/v1/extensions',
-	function( $extensions ) {
-		$extensions[] = __DIR__;
-		return $extensions;
-	}
-);
+// Check if HivePress is active
+if (!in_array('hivepress/hivepress.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    // HivePress is not active, so deactivate this plugin
+    function email_as_username_for_hivepress_error_notice() {
+        ?>
+        <div class="error notice">
+            <p><?php _e('Email as Username for HivePress requires HivePress to be installed and activated.', 'email-as-username-for-hivepress'); ?></p>
+        </div>
+        <?php
+    }
+    add_action('admin_notices', 'email_as_username_for_hivepress_error_notice');
 
-add_filter(
-	'hivepress/v1/routes',
-	function( $routes ) {
-        $routes['user_register_action'] = $routes['email_as_username_for_hivepress_register_action'];
-        unset($routes['custom_user_register_action']);
-		return $routes;
-	}
-);
+    function email_as_username_for_hivepress_deactivate() {
+        deactivate_plugins(plugin_basename(__FILE__));
+    }
+    add_action('admin_init', 'email_as_username_for_hivepress_deactivate');
+} else {
+    // Register extension directory.
+    add_filter(
+        'hivepress/v1/extensions',
+        function( $extensions ) {
+            $extensions[] = __DIR__;
+            return $extensions;
+        }
+    );
+
+    add_filter(
+        'hivepress/v1/routes',
+        function( $routes ) {
+            $routes['user_register_action'] = $routes['email_as_username_for_hivepress_register_action'];
+            unset($routes['custom_user_register_action']);
+            return $routes;
+        }
+    );
+}
